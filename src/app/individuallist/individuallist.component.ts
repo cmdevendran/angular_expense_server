16403d38b336fdd28722673c497cfbec060a76c2
Devendran <cmdevendran@gmail.com>
2024-04-28 22:58:11 +0800

    delete added

--- src/app/individuallist/individuallist.component.ts
+++ src/app/individuallist/individuallist.component.ts
@@ -6,6 +6,12 @@ import {MatCardModule} from '@angular/material/card';
 import {  NgFor } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 import {Location} from '@angular/common';
+import {nodeserver} from '../../env';
+
+import {HttpClient, HttpClientModule, HttpHeaders} from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA, OnInit, inject } from '@angular/core';
+
+
 
 
 @Component({
@@ -17,9 +23,10 @@ import {Location} from '@angular/common';
   styleUrls: ['./individuallist.component.css']
 })
 export class IndividuallistComponent {
+  httpClient = inject(HttpClient)
    obj : any;
    expenses : any;
-  constructor(private router : Router, private location : Location){
+  constructor(private router : Router, private http : HttpClient,private location : Location){
   //  console.log(this.router.getCurrentNavigation()?.extras.state.example); 
     this.obj = this.router.getCurrentNavigation()?.extras.state// should log out 'bar'
     this.expenses = this.obj.obj3
@@ -38,8 +45,19 @@ export class IndividuallistComponent {
     this.router.navigate(['/editexpense'],{state:{exp}})
   }
 
-  deleteExpense(exp:any){
+  async deleteExpense(exp:any){
     console.log("delete Expense" + exp)
+    this.http.post(nodeserver+'/expense/deleteexp/',JSON.stringify(exp),{ headers: new HttpHeaders({ 
+      'session': '5a4c50b645e2964054e516c8',
+      'Access-Control-Allow-Origin':'*',
+      'Content-Type':  'application/json',
+       }) })
+      .subscribe(data => {
+        console.log("data"+data)
+        
+  
+  
+        });
 
    
   }
